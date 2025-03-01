e9ee7ed5a9066be35f71c1d959c747f78d2b0673
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-03-01 11:02:12 +0200

    Tested the merge function in file1

--- ex01.py
+++ ex01.py
@@ -5,3 +5,17 @@ def merge_dictioaries(dict1, dict2):
         dict3[key]= value
     
     return dict3
+
+dict1 = { 
+    "a": 1,
+    "b": 2,
+    "c": 3
+}
+
+dict2 = {
+     "b": 10,
+    "d": 4
+}
+
+dict3 = merge_dictioaries(dict1, dict2)
+print (dict3)
