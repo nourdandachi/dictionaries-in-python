a4bb68b6cbc709ee8cb92394969aa28c46921c7e
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-03-01 16:57:09 +0200

    Impelented word count using a dictionary in file2

--- ex02.py
+++ ex02.py
@@ -1,3 +1,10 @@
-message= input("Please enter a sentence")
+message= input("Please enter a sentence: ")
 sentence= message.split()
+dict1 = {}
+for word in sentence:
+    if word in dict1:
+        dict1[word] +=1
+    else:
+        dict1[word] = 1
 
+print(dict1)
